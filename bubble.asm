369c269715234231af830e5b6ad8f765eadb01e8
Lars Djerf <lars.djerf@gmail.com>
2019-08-20 22:37:59 +0200

    More zeropage stuff

--- bubble.asm
+++ bubble.asm
@@ -2,66 +2,66 @@ BasicUpstart2(Entry)
 
 #import "./lib/c64.asm"
 
-Elements:
+.label ElementCount = $00fd
+.label Swap = $00fe
+
+ElementArray:
 .fill 128, random() * 128
-
-ElementCount:
-.byte * - Elements - 1  // Store len(Elements) - 1
-
-Swap:
-.byte 0
+__ElementArray:
 
 Entry:
-    lda #$01            // There's no work to be done if len(Elements) <= 1
+    lda #__ElementArray - ElementArray - 1
+    sta ElementCount        // Store len(ElementArray) - 1
+
+    lda #$01                // There's no work to be done if len(ElementArray) <= 1
     cmp ElementCount
     bpl Exit
-
-                        // Setup zeropage (0x00fb, 0x00fc) to point to Elements
-    lda #<Elements      // Low byte of address
+                            // Setup zeropage (0x00fb, 0x00fc) to point to ElementArray
+    lda #<ElementArray      // Low byte of address
     sta $00fb
-    lda #>Elements      // High byte of address
+    lda #>ElementArray      // High byte of address
     sta $00fc
 
 !Outer:
+    lda #$00                // Reset swap flag
+    sta Swap
+
     ldx ElementCount
     ldy #$00
 
 !Inner:
     lda ($fb), y
     iny
-    cmp ($fb), y        // Compare Elements[y] to Elements[y + 1]
-    bmi !SkipSwap+      // Swap if Elements[y] > Elements[y + 1]
+    cmp ($fb), y            // Compare ElementArray[y] to ElementArray[y + 1]
+    bmi !SkipSwap+          // Swap if ElementArray[y] > ElementArray[y + 1]
 
     pha
-    lda ($fb), y        // Load Elements[y + 1]
+    lda ($fb), y            // Load ElementArray[y + 1]
     dey
-    sta ($fb), y        // Set Elements[y] := Elements[y + 1]
+    sta ($fb), y            // Set ElementArray[y] := ElementArray[y + 1]
     iny
     pla
-    sta ($fb), y        // Set Elements[y + 1] := Elements[y]
+    sta ($fb), y            // Set ElementArray[y + 1] := ElementArray[y]
 
-    WaitForVsync()      // Let's waste some cycles (312 * 63?)
+    WaitForVsync()          // Let's waste some cycles (312 * 63?)
     stx VIC.BORDER_COLOR
 
     lda #$01
-    sta Swap            // Set swap flag to true
+    sta Swap                // Set swap flag to true
 
 !SkipSwap:
     dex
     bne !Inner-
 
-    lda #$00            // If no swap was performed we're done
+    lda #$00                // If no swap was performed we're done
     cmp Swap
     beq Exit
-
-    lda #$00            // Reset swap flag
-    sta Swap
 
     dec ElementCount
     bne !Outer-
 
 Exit:
-    lda #$0e           // Reset background color
+    lda #$0e              // Reset background color
     sta VIC.BORDER_COLOR
 
     rts
